130311be0bc8da5164ac9c73bc93f2dd266e4b97
Luis A <luis_alejandro.az@outlook.com>
2022-03-01 10:05:07 -0400

    feat: creando proyecto censo para ciudad salud

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import 'dotenv/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  await app.listen(process.env.SERVER_PORT);
+  console.info(`Server run on port: ${process.env.SERVER_PORT}`);
+}
+bootstrap();
