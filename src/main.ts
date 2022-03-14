c2bfbc853c1e16ee5c08b0d50375681ec1686577
Coxxio <antoniocortez1497@gmail.com>
2022-03-14 10:14:04 -0400

    a

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(process.env.SERVER_PORT);
+  app.useGlobalPipes(new ValidationPipe());
   console.info(`Server run on port: ${process.env.SERVER_PORT}`);
 }
 bootstrap();
--- src/person/dto/create-person.dto.ts
+++ src/person/dto/create-person.dto.ts
@@ -2,6 +2,8 @@ import { IsDate, IsString } from "class-validator";
 import { Gender } from "../enum/person.enum";
 
 export class CreatePersonDto {
+    id?: number;
+
     @IsString()
     dni: string;
 
